98cc2e5e3081170e11d0c43d1bc67352107398dc
ThatTwoDude <73597082+ThatTwoDude@users.noreply.github.com>
2022-02-23 10:44:28 -0700

    sa

--- 3D_Archery/Assets/Scripts/ArcherController.cs
+++ 3D_Archery/Assets/Scripts/ArcherController.cs
@@ -6,11 +6,18 @@ public class ArcherController : MonoBehaviour
 {
     Animator anim;
     Rigidbody rb;
+
+    [SerializeField]
+    GameObject spawnPoint;
+    [SerializeField]
+    GameObject projectilePrefab;
+    [SerializeField]
+    float arrow_speed = 1;
     // Start is called before the first frame update
     void Start()
     {
         Animator anim = GetComponent<Animator>();
-        rb = GetComponentInParent<Rigidbody>();
+        rb = GetComponentInParent<Rigidbody>()
     }
 
     // Update is called once per frame
@@ -23,26 +30,46 @@ public class ArcherController : MonoBehaviour
             anim.SetBool("Walk", false);
             anim.SetBool("Run", true);
         }
+
         else if (Input.GetAxisRaw("Vertical") != 0 && Input.GetKey(KeyCode.LeftShift))
         {
             //anim.SetTrigger("Attack");
             anim.SetBool("Walk", true);
             anim.SetBool("Run", false);
         }
+
         else
         {
             anim.SetBool("Walk", false);
             anim.SetBool("Run", false);
         }
-        if (Input.GetAxisRaw("Fire1") > 0)
+
+        if (Input.GetKeyDown(KeyCode.F))
         {
-            //spawn an arrow
+            anim.SetTrigger("Attack");
+            
         }
     }
 
-    private void OnTriggerrEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        anim.SetTrigger("Die");
-        rb.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionX;
+        if(other.tag == "poison mist")
+        {
+            anim.SetTrigger("Die");
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            ThirdPersonCharacter characterScript = GetComponentInParent<ThirdPersonCharacter>();
+            characterScript.m_MovingTurnSpeed = 0;
+            characterScript.m_StationaryTurnSpeed = 0;
+        }
+    }
+
+    public void FireArrow()
+    {
+        //spawn an arrow
+        GameObject arrow = Instantiate(projectilePrefab, spawnPoint.transform.position, spawnPoint.transform.rotation);
+
+        Rigidbody arrow_rb = arrow.GetComponent<Rigidbody>();
+        arrow_rb.velocity = spawnPoint.transform.forward * arrow_speed;
+        Destroy(arrow, 5);
     }
 }
